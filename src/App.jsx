6ee6f06c00817ed63226fb4dde7ab7cb13f13eb7
fajrigans <fajrialfatih012@gmail.com>
2025-06-03 18:52:56 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -480,7 +480,7 @@ const App = () => {
               ))}
             </nav>
           </header>
-
+          <script type='text/javascript' src='https://edge-cdn.trakteer.id/js/trbtn-overlay.min.js?v=14-05-2025'></script><script type='text/javascript' class='troverlay'>(function() {var trbtnId = trbtnOverlay.init('Dukung Saya di Trakteer','#be1e2d','https://trakteer.id/ahmadrizkyalfajri/tip/embed/modal','https://edge-cdn.trakteer.id/images/embed/trbtn-icon.png?v=14-05-2025','40','inline');trbtnOverlay.draw(trbtnId);})();</script>
           <main className="flex-grow flex items-center justify-center p-6">
             <AnimatedRoutes />
           </main>
